1fb10b214f737b3a15663a37048257664e49c1ee
KARTHI <adept1234@grr.la>
2014-11-11 22:51:55 +0530

    Adding OnCreate Method

--- TestingApp/src/com/example/testingapp/Main.java
+++ TestingApp/src/com/example/testingapp/Main.java
@@ -1,8 +1,22 @@
 package com.example.testingapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class Main extends Activity {
-
+	Context context=Main.this;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	
+	TextView text=new TextView(context);
+	text.setText("Testing CI in Jenkines");
+	text.setTextColor(Color.RED);
+	setContentView(text);
+}
 }
