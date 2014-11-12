794e477b9d4525532e8336e8e016ecbeafab27b9
Karthik.k <Karthik.k@Adeptpros-D-23.adeptpros.com>
2014-11-12 12:46:29 +0530

    Adding Data

--- TestingApp/src/com/example/testingapp/Main.java
+++ TestingApp/src/com/example/testingapp/Main.java
@@ -3,6 +3,7 @@ package com.example.testingapp;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.LinearLayout;
@@ -17,10 +18,15 @@ protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	LinearLayout layout=new LinearLayout(context);
 	layout.setGravity(Gravity.CENTER);
+	layout.setOrientation(LinearLayout.VERTICAL);
 	TextView text=new TextView(context);
 	text.setText("Testing CI in Jenkines");
 	text.setTextColor(Color.RED);
 	layout.addView(text);
+	TextView text1=new TextView(context);
+	text1.setText("Developer Karthik");
+	text1.setTextColor(Color.GREEN);
+	layout.addView(text1);
 	setContentView(layout);
 }
 }
